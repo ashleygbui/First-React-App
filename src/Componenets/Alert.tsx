23f39ce5a3934a2470cdef37fd88051cff119409
ashleygbui <ashleyb.ui@berkeley.edu>
2023-06-07 16:26:19 -0700

    complete

--- src/Componenets/Alert.tsx
+++ src/Componenets/Alert.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const Alert = ({ children, onClose }: Props) => {
   return (
-    <div className="alert alert-primary alert-dismissibl">
+    <div className="alert alert-primary alert-dismissible">
       {children}
       <button
         type="button"
